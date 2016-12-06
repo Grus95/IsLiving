2938ec0daa5e8b2afd88eb528f202efbe7a732d0
grus <1249686063@qq.com>
2016-12-06 22:34:37 +0800

    添加个TVshowRecyclerFragment，模仿recylefragment创建新的在codelibrary

---
+++ codelibrary/src/main/java/com/example/grus95/codelibrary/test_activity/TVShowTool/TVShowRecyclerFragment.java
@@ -0,0 +1,12 @@
+package com.example.grus95.codelibrary.test_activity.TVShowTool;
+
+import com.example.grus95.baselibrary.recycler_refresh_layout.realization_demo.demo.RecyclerFragment;
+import com.example.grus95.baselibrary.recycler_refresh_layout.realization_demo.model.OpenProjectModel;
+
+/**
+ * Created by grus95 on 2016/12/6.
+ */
+
+public class TVShowRecyclerFragment {
+
+}
